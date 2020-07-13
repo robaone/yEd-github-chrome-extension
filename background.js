192846c1a90f89d75f110344a22bfa0e9eae0f06
Ansel Robateau <ansel@casechek.com>
2020-07-12 23:10:37 -0500

    Add regex for page (#10)
    * Add regex for page
    
    * feat: Recognize .graphml pages

--- background.js
+++ background.js
@@ -11,7 +11,9 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {
+              urlMatches: 'github.com/([^/]+)\/([^/]+)\/blob\/([^/]+\/([^/]+\/)*[^/]+.graphml)'
+          },
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
