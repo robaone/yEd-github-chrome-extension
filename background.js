0dc7afbc6ab075feac60fbb2fa8a34c2a7ccea8e
Ansel Robateau <ansel@casechek.com>
2020-07-12 22:47:08 -0500

    feat: Add url storage, UI and app icons

--- background.js
+++ background.js
@@ -5,7 +5,16 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({url: ''}, function() {
-    console.log("The url is not set");
-  });
+    chrome.storage.sync.set({url: 'not set'}, function() {
+      console.log("The url is not set");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
 });
