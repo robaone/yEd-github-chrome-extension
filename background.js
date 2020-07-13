8c072eb907fca9ecb2bf64dc1358ffe5bb2a3b82
Ansel Robateau <ansel@casechek.com>
2020-07-12 23:03:44 -0500

    Add regex for page

--- background.js
+++ background.js
@@ -11,7 +11,10 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {
+              hostEquals: 'github.com',
+              urlMatches: '([^/]+)\/([^/]+)\/blob\/([^/]+\/([^/]+\/)*[^/]+.graphml)'
+          },
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
